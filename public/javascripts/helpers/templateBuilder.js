63fc232e4a6a87db388a0da774bcf1a27c53011f
Rene Rubalcava <odoe@odoe.net>
2013-04-26 06:55:58 -0700

    Update templateBuilder.js
    Added options parameter to accept options for URL field info.

--- public/javascripts/helpers/templateBuilder.js
+++ public/javascripts/helpers/templateBuilder.js
@@ -5,7 +5,7 @@
 (function() {
     'use strict';
 
-    define([], function() {
+    define(['esri/InfoTemplate'], function(InfoTemplate) {
 
         var _builder = {};
 
@@ -14,10 +14,13 @@
         * graphic feature displaying all attributes in a table format.
         * @param {esri.Graphic} feature Graphic to set infoTemplate.
         * @return {esri.Graphic} Graphic with infoTemplate set.
-*/
-        _builder.buildInfoTemplate = function(feature) {
+        */
+        _builder.buildInfoTemplate = function(feature, options) {
 
-            var content = [];
+
+            var content = [],
+                urlField = options ? options.urlField : '',
+                urlPrefix = options ? options.urlPrefix : '';
             // get the dom element for my infotemplate as a string
             content[content.length] = '<table cellspacing="0" class="table table-striped table-condensed attr-info">';
             if (feature.layerName) {
@@ -37,12 +40,16 @@
                 name;
                 if (!(_key.indexOf('shape') > -1 || _key === 'layername'|| _key === 'objectid' || _key === 'fid')) {
                     name = keys[i];
-                    content[content.length]= '<tr><td class="fieldName">' + name + '</td><td>${' + name + '}</td></tr>';
+                    if (name.toLowerCase() !== urlField) {
+                        content[content.length]= '<tr><td class="fieldName">' + name + '</td><td>${' + name + '}</td></tr>';
+                    } else {
+                        content[content.length] = '<tr><td class="fieldName">' + name + '</td><td><a href="' + urlPrefix + '${' + name + '}">${' + name + '}</a></td></tr>';
+                    }
                 }
             }
             content[content.length] = '</table>';
             // now set the template
-            feature.setInfoTemplate( new esri.InfoTemplate('', content.join('')) );
+            feature.setInfoTemplate( new InfoTemplate('', content.join('')) );
 
             return feature;
 
@@ -53,4 +60,3 @@
     });
 
 }).call(this);
-
